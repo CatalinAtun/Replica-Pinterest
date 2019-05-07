772ce0abf972b42ba2f5b483621c42efb243b798
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-05-07 17:25:53 -0400

    Updates

---
+++ precache-manifest.de70711e25ef6b681c87f07fd661a251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfb04e62210c9800a8790fed24ce818",
+    "url": "/Replica-Pinterest/index.html"
+  },
+  {
+    "revision": "af5967b328c014962425",
+    "url": "/Replica-Pinterest/static/css/main.fc86ca07.chunk.css"
+  },
+  {
+    "revision": "5ef879d470a33d6ca76d",
+    "url": "/Replica-Pinterest/static/js/2.f040f978.chunk.js"
+  },
+  {
+    "revision": "af5967b328c014962425",
+    "url": "/Replica-Pinterest/static/js/main.ae9c0c0c.chunk.js"
+  },
+  {
+    "revision": "acb2d82bda962664eee7",
+    "url": "/Replica-Pinterest/static/js/runtime~main.769c0aff.js"
+  },
+  {
+    "revision": "899cd5e1bc97ea4c94c0b088b0fa23d8",
+    "url": "/Replica-Pinterest/static/media/logo.899cd5e1.png"
+  }
+]);
